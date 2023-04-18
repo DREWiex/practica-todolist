0e48ae34b61cc13944328676a9c3f69b104dd402
DREWiex <aleon88@gmail.com>
2023-04-18 22:46:32 +0200

    Refact de hook useForm

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -3,23 +3,9 @@ import { useState } from "react"
 export const useForm = (estadoInicial) => {
 
     const [form, setForm] = useState(estadoInicial);
+    
     //const [sent, setSent] = useState(false)
 
-    // const serialiceForm = (form) => {
-
-    //     const formData = new FormData(form)
-
-    //     const data = {}
-
-    //     for (let [key, value] of formData) {
-    //         console.log(key, value)
-    //         data[key] = value
-
-    //     }
-
-    //     return data
-
-    // }
 
     const handleChange = ({ target }) => {
 
@@ -36,11 +22,7 @@ export const useForm = (estadoInicial) => {
 
 
     return {
-        form,
-        setForm,
-        handleChange,
+        handleChange
     };
 
 };
-
-
